c5cd502c36d89b1ce619524d443b2fb114a75d04
vanhogo123 <ntav2408@gmail.com>
2023-06-08 08:43:47 +0700

    tuan4

---
+++ Nguyen Thi Ai Van_2087700053/Migrations/202306050256012_dataCourse.cs
@@ -0,0 +1,16 @@
+﻿namespace Nguyen_Thi_Ai_Van_2087700053.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dataCourse : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
